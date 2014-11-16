eaa39ba8846ce139393be4342bac3196569b8a99
Alex Turok <a.o.turok@gmail.com>
2014-11-17 02:38:47 +0300

    For RK separated the coefficients calc and fixed the approximation

--- src/elegantchaos/math.clj
+++ src/elegantchaos/math.clj
@@ -7,23 +7,27 @@
 (defn timesvec [k v]
 	(map (partial * k) v))
 
+(defn rk4-coeffs [f x t dt]
+	(let [hdt (* 0.5 dt)
+		  k1 (f t x)
+		  k2 (f (+ t hdt) (addvec x (map (partial * hdt) k1)))
+		  k3 (f (+ t hdt) (addvec x (map (partial * hdt) k2)))
+		  k4 (f (+ t  dt) (addvec x (map (partial *  dt) k3)))]
+		[k1 k2 k3 k4]))
+
 (defn rk4-samestep [fdots dt]
 	(let [f (apply juxt fdots)]
 		(fn [[t x0]]
-			(let [hdt (* 0.5 dt)
-				  k1 (f t x0)
-				  k2 (f (+ t hdt) (addvec x0 (map (partial * hdt) k1)))
-				  k3 (f (+ t hdt) (addvec x0 (map (partial * hdt) k2)))
-				  k4 (f (+ t  dt) (addvec x0 (map (partial *  dt) k3)))]
+			(let [[k1 k2 k3 k4] (rk4-coeffs f x0 t dt)]
 				[(+ t dt)
 				 (addvec
 				 	x0
 				 	(timesvec
-				 		(/ 1.0 6.0)
+				 		(* dt (/ 1.0 6.0))
 				 		(addvec
 				 			k1
-				 			(timesvec 0.5 k2)
-				 			(timesvec 0.5 k3)
+				 			(timesvec 2.0 k2)
+				 			(timesvec 2.0 k3)
 				 			k4)))]))))
 
 (defn iter [f x0 dt n]
