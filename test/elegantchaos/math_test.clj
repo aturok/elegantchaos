f76325133544cd4fb5fdc30940cf171a37b7f895
Alex Turok <a.o.turok@gmail.com>
2014-11-17 02:59:39 +0300

    In math-test factored out almost= from valmost=

--- test/elegantchaos/math_test.clj
+++ test/elegantchaos/math_test.clj
@@ -4,12 +4,15 @@
 
 (defn ivp [t [y]] (- 1 (* t y)))
 
-(defn almost= [v1 v2]
-	(every? (fn [[a b]] (<= (Math/abs (- a b)) 0.0001))
+(defn almost= [a b]
+	(<= (Math/abs (- a b)) 0.0001))
+
+(defn valmost= [v1 v2]
+	(every? (partial apply almost=)
 		(map vector v1 v2)))
 
 (deftest rk4-coefficients-checks
   (testing "y' = 1 - t*y at 1.0 with y(0) = 1.0"
-    (is (almost= (map first (rk4-coeffs (juxt ivp) [1.0] 0.0 1.0)) [1.0 0.25 0.4375 -0.4375])))
+    (is (valmost= (map first (rk4-coeffs (juxt ivp) [1.0] 0.0 1.0)) [1.0 0.25 0.4375 -0.4375])))
   (testing "y' = 1 - t*y at 1.0 with y(0) = 1.0"
-    (is (almost= (map first (rk4-coeffs (juxt ivp) [1.0] 0.0 0.5)) [1.0 0.6875 0.70703125 0.3232421875]))))
+    (is (valmost= (map first (rk4-coeffs (juxt ivp) [1.0] 0.0 0.5)) [1.0 0.6875 0.70703125 0.3232421875]))))
