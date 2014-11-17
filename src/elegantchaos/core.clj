b4fa65db9cbdd4077e71372dbb658831afd1673d
Alex Turok <a.o.turok@gmail.com>
2014-11-18 00:34:49 +0300

    Separated trajectory calc from rendering

--- src/elegantchaos/core.clj
+++ src/elegantchaos/core.clj
@@ -9,13 +9,18 @@
   (background 255)
   (stroke 00))
 
+(def scalef 100.0)
+
+(def traj
+    (->> (iter chua/equations [0.7 0.0 0.0] 0.01 5000)
+        (map fnext)
+        (map (partial timesvec scalef))))
+
 (defn draw []
   (background 255)
-  (translate (/ (width) 2) (/ (height) 2) 0)
-  (let [traj-with-time (iter chua/equations [0.7 0.0 0.0] 0.01 100)
-        traj (map fnext traj-with-time)]
-    (dorun
-     (map #(apply line %) (line-join-points traj)))))
+  (translate (/ (width) 2) (/ (height) 2) 5)
+  (dorun
+    (map #(apply line %) (line-join-points traj))))
 
 (defsketch trajectory
   :title "Chua"
