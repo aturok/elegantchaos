b3c1b025d432a7eca4c01187b4c32f441e496a4e
Alex Turok <a.o.turok@gmail.com>
2014-11-17 03:03:24 +0300

    Added simple checks for RK4

--- test/elegantchaos/math_test.clj
+++ test/elegantchaos/math_test.clj
@@ -16,3 +16,9 @@
     (is (valmost= (map first (rk4-coeffs (juxt ivp) [1.0] 0.0 1.0)) [1.0 0.25 0.4375 -0.4375])))
   (testing "y' = 1 - t*y at 1.0 with y(0) = 1.0"
     (is (valmost= (map first (rk4-coeffs (juxt ivp) [1.0] 0.0 0.5)) [1.0 0.6875 0.70703125 0.3232421875]))))
+
+(deftest rk4-test
+  (testing "y' = 1 - t*y at 1.0 with y(0) = 1.0"
+    (is (valmost= (fnext (fnext (iter [ivp] [1.0] 1.0 2))) [1.3229166667])))
+  (testing "y' = 1 - t*y at 1.0 with y(0) = 1.0"
+    (is (valmost= (fnext (fnext (iter [ivp] [1.0] 0.5 2))) [1.342692057]))))
